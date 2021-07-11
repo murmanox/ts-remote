42534d34ee7211433a1dc2a0a07ed56513f29578
Murmanox <murmanox@hotmail.com>
2021-07-11 20:53:04 +0100

    no change pog

--- typings/NetworkEvent.d.ts
+++ typings/NetworkEvent.d.ts
@@ -1,19 +1,19 @@
 import { PrependPlayerToArgs, FunctionArguments } from "./function-types";
 
 interface EventParams {
-    onClientEvent: (...args: any[]) => any;
-    onServerEvent: (...args: any[]) => any;
+	onClientEvent: (...args: any[]) => any;
+	onServerEvent: (...args: any[]) => any;
 }
 
 interface NetworkEvent<T extends EventParams> {
-    onClientEvent: RBXScriptSignal<T["onClientEvent"]>;
-    onServerEvent: RBXScriptSignal<PrependPlayerToArgs<T["onServerEvent"]>>;
+	onClientEvent: RBXScriptSignal<T["onClientEvent"]>;
+	onServerEvent: RBXScriptSignal<PrependPlayerToArgs<T["onServerEvent"]>>;
 
-    fireAllClients(...args: FunctionArguments<T["onClientEvent"]>): void;
-    fireClient(player: Player, ...args: FunctionArguments<T["onClientEvent"]>): void;
-    fireServer(...args: FunctionArguments<T["onServerEvent"]>): void;
+	fireAllClients(...args: FunctionArguments<T["onClientEvent"]>): void;
+	fireClient(player: Player, ...args: FunctionArguments<T["onClientEvent"]>): void;
+	fireServer(...args: FunctionArguments<T["onServerEvent"]>): void;
 }
 
 export interface NetworkEventConstructor {
-    new <T extends EventParams>(name: string): NetworkEvent<T>;
+	new <T extends EventParams>(name: string): NetworkEvent<T>;
 }
