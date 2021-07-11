6d0da9b63ca6fb0e55e6ef91ebcd00bcaf54870f
Murmanox <murmanox@hotmail.com>
2021-07-11 20:53:49 +0100

    fix issue with invokeServer type

--- typings/NetworkFunction.d.ts
+++ typings/NetworkFunction.d.ts
@@ -1,29 +1,31 @@
-import { PrependPlayerToArgs } from "./function-types";
+import { PrependPlayerToArgs, PrependThisToArgs } from "./function-types";
 
 interface FunctionParams {
-    onClientInvoke?: (...args: any[]) => any;
-    onServerInvoke: (...args: any[]) => any;
+	onClientInvoke?: (...args: any[]) => any;
+	onServerInvoke: (...args: any[]) => any;
 }
 
+type OnClientInvoke<T extends FunctionParams> = T["onClientInvoke"];
+type OnServerInvoke<T extends FunctionParams> = T["onServerInvoke"];
 interface NetworkFunction<T extends FunctionParams> {
-    /**
-     * Don't use this. It's an anti-pattern.
-     */
-    onClientInvoke: T["onClientInvoke"];
-    onServerInvoke: PrependPlayerToArgs<T["onServerInvoke"]>;
+	/**
+	 * Don't use this. It's an anti-pattern.
+	 */
+	onClientInvoke: T["onClientInvoke"];
+	onServerInvoke: PrependPlayerToArgs<T["onServerInvoke"]>;
 
-    /**
-     * Calls the method bound to the NetworkFunction by NetworkFunction.onServerInvoke.
-     * Can only be called from the client.
-     */
-    readonly invokeServer: T["onServerInvoke"];
+	/**
+	 * Calls the method bound to the NetworkFunction by NetworkFunction.onServerInvoke.
+	 * Can only be called from the client.
+	 */
+	readonly invokeServer: PrependThisToArgs<T["onServerInvoke"], this>;
 
-    /**
-     * Don't use this. It's an anti-pattern.
-     */
-    readonly invokeClient: PrependPlayerToArgs<T["onClientInvoke"]>;
+	/**
+	 * Don't use this. It's an anti-pattern.
+	 */
+	readonly invokeClient: PrependThisToArgs<PrependPlayerToArgs<T["onClientInvoke"]>, this>;
 }
 
 export interface NetworkFunctionConstructor {
-    new <T extends FunctionParams>(name: string): NetworkFunction<T>;
+	new <T extends FunctionParams>(name: string): NetworkFunction<T>;
 }
