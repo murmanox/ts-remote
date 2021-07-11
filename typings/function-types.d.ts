8bc969a7a246aad5f139362a093e7bb588eb4c89
Murmanox <murmanox@hotmail.com>
2021-07-11 20:53:22 +0100

    prepend this utility type

--- typings/function-types.d.ts
+++ typings/function-types.d.ts
@@ -1,5 +1,8 @@
 export type FunctionArguments<T> = T extends (...args: infer U) => void ? U : [];
 export type ReturnType<T> = T extends (...args: any[]) => infer U ? U : any;
 export type PrependPlayerToArgs<T> = T extends (...args: any[]) => any
-    ? (player: Player, ...args: FunctionArguments<T>) => ReturnType<T>
-    : never;
+	? (player: Player, ...args: FunctionArguments<T>) => ReturnType<T>
+	: never;
+export type PrependThisToArgs<T, U> = T extends (...args: any[]) => any
+	? (this: U, ...args: FunctionArguments<T>) => ReturnType<T>
+	: never;
